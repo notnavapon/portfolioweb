9fdae4c71291ca39df4d017d79222577252c9ed8
notnavapon <notnavapon.nnk@gmail.com>
2025-09-21 01:35:38 +0700

    project v1 port and chat complete[fix4.2]

--- Server/src/index.js
+++ Server/src/index.js
@@ -37,7 +37,7 @@ app.use("/api/messages" , messageRoutes);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../Client/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/{*any}", (req, res) => {
         res.sendFile(path.join(__dirname, "../Client", "dist", "index.html"));
     });
 }
